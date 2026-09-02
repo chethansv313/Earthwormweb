5d4a6a150f620b90c174ec3d85cab29608e33306
chethansv313 <chethansv13@gmail.com>
2026-09-03 03:45:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -546,7 +546,8 @@ def price_crop(crop_name):
 # ---------------------------------------------------------------
 # SERVER ENTRY POINT
 # ---------------------------------------------------------------
-if __name__ == "__main__":
-  with app.app_context():
-    db.create_all()
-  app.run(debug=True, port=5000)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
